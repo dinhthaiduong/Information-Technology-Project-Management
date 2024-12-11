90a0a5cb93e9de146db99c659b6c96fd030673c1
khuong <dinhquangkhuong5566@gmail.com>
2024-12-11 16:35:35 +0700

    fix docker file

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ source .venv/bin/activate
 $docker compose up -d
 
 python script.py data/all_context2.json
+# python script.py data/book.txt
 
 mkdir -p result
 docker_volume_saved=$($docker volume list | grep script-neo4j | cut -f8 -d' ')
